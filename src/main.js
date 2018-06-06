cb3a239e79f53d45526843f42badbb9677a71b6f
macheng2017 <gos360@163.com>
2018-06-06 15:36:09 +0800

    底部导航

--- src/main.js
+++ src/main.js
@@ -17,6 +17,28 @@ export default {
       'navigationBarBackgroundColor': '#fff',
       'navigationBarTitleText': 'WeChat',
       'navigationBarTextStyle': 'black'
+    },
+    'tabBar': {
+      'selectedColor': '#EA5149',
+      'list': [{
+        'pagePath': 'pages/books/main',
+        'text': '书籍',
+        'iconPath': 'static/img/book.png',
+        'selectedIconPath': 'static/img/book-active.png'
+      },
+      {
+        'pagePath': 'pages/comments/main',
+        'text': '评论',
+        'iconPath': 'static/img/todo.png',
+        'selectedIconPath': 'static/img/todo-active.png'
+      },
+      {
+        'pagePath': 'pages/me/main',
+        'text': '我的信息',
+        'iconPath': 'static/img/me.png',
+        'selectedIconPath': 'static/img/me-active.png'
+      }
+      ]
     }
   }
 }
