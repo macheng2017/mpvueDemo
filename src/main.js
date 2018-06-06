9541ed1cee36a411e3f871a540974470b9d2b9c7
macheng2017 <gos360@163.com>
2018-06-06 15:15:40 +0800

    初始化三个空页面

--- src/main.js
+++ src/main.js
@@ -11,13 +11,12 @@ app.$mount()
 // 配置小程序显示界面
 export default {
   config: {
-    pages: ['pages/me/main'],
+    pages: ['pages/books/main', 'pages/me/main', '^pages/comments/main'],
     'window': {
       'backgroundTextStyle': 'light',
       'navigationBarBackgroundColor': '#fff',
       'navigationBarTitleText': 'WeChat',
       'navigationBarTextStyle': 'black'
     }
-
   }
 }
---
+++ src/pages/comments/index.vue
@@ -0,0 +1,11 @@
+<template>
+<div>
+  评论页面
+</div>
+
+</template>
+<script>
+export default {}
+</script>
+<style>
+</style>
