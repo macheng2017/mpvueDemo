059f6ac1483127125690ae90734aacb146ccf519
macheng2017 <gos360@163.com>
2018-06-06 16:39:43 +0800

    用promise封装了wx函数test async

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 
 <script>
-import config from './config'
+import * as utils from './utils'
 export default {
-  created () {
-    wx.request({
-      url: config.host + '/weapp/test', // 仅为示例，并非真实的接口地址
-      data: {
-        // x: '',
-        // y: ''
-      },
-      success: function (res) {
-        console.log(res.data)
-      }
-    })
+  async created () {
+    const res = await utils.get('/weapp/test')
+    // wx.request({
+    //   url: config.host + '/weapp/test', // 仅为示例，并非真实的接口地址
+    //   data: {
+    //     // x: '',
+    //     // y: ''
+    //   },
+    //   success: function (res) {
+    //     console.log(res.data)
+    //   }
+    // })
+    console.log(res)
   }
 }
 </script>
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,17 @@
 //  工具函数
+import config from './config'
+// 为了能使用 async await 把wx.request 封装下
+export function get (url) {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: config.host + url,
+      success: function (res) {
+        if (res.data.code === 0) {
+          resolve(res.data.data)
+        } else {
+          reject(res.data)
+        }
+      }
+    })
+  })
+}
