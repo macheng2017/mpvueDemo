3cf6d0b77b9bd0cb8f53fa71c0f00d17e47d7c51
macheng2017 <gos360@163.com>
2018-06-06 15:56:55 +0800

    小程序提供api用法测试

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 
 <script>
+import config from './config'
 export default {
-
+  created () {
+    wx.request({
+      url: config.host + '/weapp/test', // 仅为示例，并非真实的接口地址
+      data: {
+        // x: '',
+        // y: ''
+      },
+      success: function (res) {
+        console.log(res.data)
+      }
+    })
+  }
 }
 </script>
 
 <style>
-
 </style>
