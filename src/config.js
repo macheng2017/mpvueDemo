4fb804da217b382d750cf34eb9b65f461d1113f9
macheng2017 <gos360@163.com>
2018-06-10 11:15:49 +0800

    演示修改openId获取不到的方法

--- src/config.js
+++ src/config.js
@@ -3,6 +3,8 @@ const host = 'http://localhost:5757'
 
 const config = {
   host,
-  loginUrl: `${host}/weapp/login`
+  loginUrl: `${host}/weapp/login`,
+  // 为解决上个版本sdk-client 没有openId问题自己调用下后端获取
+  userUrl: `${host}/weapp/user`
 }
 export default config
--- src/pages/me/index.vue
+++ src/pages/me/index.vue
@@ -41,7 +41,17 @@ export default {
             // 存在浏览器中
             wx.setStorageSync('userInfo', userInfo)
             this.userInfo = userInfo
-            // 为了解决上个
+            // 为了解决上个sdk中返回值没有带openId 当然现在'wafer2-client-sdk'已经解决了这个问题
+            // 利用openId来判断是否放权
+            // 自己调用server目录下的
+            // qcloud.request({
+            //   url: config.userUrl,
+            //   login: true,
+            //   success: userReq => {
+            //     wx.setStorageSync('userInfo', userReq.data.data)
+            //     this.userInfo = userReq.data.data
+            //   }
+            // })
           },
           fail: function(err) {
             console.log('登录失败', err)
