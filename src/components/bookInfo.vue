49955426082586000df7ac99d9a611d0fd694874
macheng2017 <gos360@163.com>
2018-06-14 18:00:13 +0800

    图书详情 图书封面

--- src/components/bookInfo.vue
+++ src/components/bookInfo.vue
@@ -1,6 +1,11 @@
 <template lang="pug">
-.container
-  img(:src="info.image")
+.bookInfo
+  .thumb
+    img( class="back" :src="info.image" mode="aspectFill")
+    img( class="img" :src="info.image" mode="aspectFit")
+  .info
+    .title {{info.title}}
+    .author {{info.author}}
 </template>
 <script>
 export default {
@@ -11,5 +16,34 @@ export default {
 }
 </script>
 <style lang="sass" scoped>
+.bookInfo
+  .thumb
+    width: 750rpx
+    height: 500rpx
+    overflow: hidden
+    position: relative
+    .back
+      filter: blur(15px)
+      width: 100%
+    .img
+      position: absolute
+      width: 100%
+      heigth: 300rpx
+      top: 30rpx
+      left: 0
+  .info
+    color: white
+    position: absolute
+    width: 100%
+    left: 0
+    top: 330rpx
+    text-align: center
+    .title
+      font-size: 20px
+    .author
+      font-size: 14px
+
+
+
 
 </style>
--- src/pages/detail/index.vue
+++ src/pages/detail/index.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.container {{bookid}}
+.container
   BookInfo(:info="info")
 </template>
 
