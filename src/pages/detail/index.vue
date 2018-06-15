3d0f5df110248766ac6e814b995d72c9bec16cd3
macheng2017 <gos360@163.com>
2018-06-15 18:29:37 +0800

    使用百度地图api获取地理位置

--- src/pages/detail/index.vue
+++ src/pages/detail/index.vue
@@ -39,7 +39,34 @@ export default {
       })
       this.info = info
     },
-    getGeo() {},
+    getGeo(e) {
+      let url = 'http://api.map.baidu.com/geocoder/v2/'
+      if (e.target.value) {
+        wx.getLocation({
+          // FgAwGxsSSYuD96qXuHXfpqk7dGnwVMab 百度地图token
+          // http://api.map.baidu.com/geocoder/v2/?callback=renderReverse&location=35.658651,139.745415&output=json&pois=1&ak=您的ak //GET请求
+          success: geo => {
+            wx.request({
+              url,
+              data: {
+                location: `${geo.latitude}, ${geo.longitude}`,
+                output: 'json',
+                ak: 'FgAwGxsSSYuD96qXuHXfpqk7dGnwVMab'
+              },
+              success: res => {
+                console.log(res)
+                if (res.data.status === 0) {
+                  this.location = res.data.result.addressComponent.city
+                } else {
+                }
+              }
+            })
+          }
+        })
+      } else {
+        this.location = ''
+      }
+    },
     // mpvue 对获取信息的属性有有修改 e.detail.value => e.target.value
     getPhone(e) {
       if (e.target.value) {
